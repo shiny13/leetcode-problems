2bc65f82903ec9640f23bba8a367133385f94f8e
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-04-06 23:49:57 +1000

    Self dividing number

---
+++ SelfDividingNumbers/self_dividing_numbers.py
@@ -0,0 +1,29 @@
+import sys 
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    bound = list(map(int, input.split()))
+    left = bound[0]
+    right = bound[1]
+
+    output = []
+    for i in range(left, right+1):
+        if i < 10:
+            output.append(i)
+            continue
+        res = set(map(int, str(i)))
+        add = -1
+        for r in res:
+            if r == 0:
+                break 
+            if i % r == 0:
+                add = 0
+            else:
+                add = -1
+                break        
+        if add == -1:
+            continue
+        output.append(i)
+    
+    print(output)
+    #return output
