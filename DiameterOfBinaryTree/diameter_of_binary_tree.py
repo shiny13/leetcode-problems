bee38ff042630d14223f87b2bbf40b8bfaafff46
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-04-12 01:51:03 +1000

    Diameter of binary tree

---
+++ DiameterOfBinaryTree/diameter_of_binary_tree.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        return 0
+
+if __name__ == '__main__':
+    sol = Solution()
