1309f88302b68b5a59e20bb1387635a3a323c153
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-04-19 19:33:28 +1000

    Improved time complexity

--- SearchInRotatedSortedArray/search_in_rotated_sorted_array.py
+++ SearchInRotatedSortedArray/search_in_rotated_sorted_array.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        """
         index = -1
         for i in range(len(nums)):
             if target == nums[i]:
@@ -10,6 +11,31 @@ class Solution:
                 break
         print(index)
         return index
+        """
+        n = len(nums)
+
+        def find(l, r):
+            if l > r:
+                return -1
+            if nums[l] <= nums[r]:
+                if nums[l] <= target <= nums[r]:
+                    # binary search here
+                    while l <= r:
+                        m = (l + r) // 2
+                        if target == nums[m]:
+                            return m
+                        elif target > nums[m]:
+                            l = m+1
+                        else:
+                            r = m-1
+                return -1
+            elif target >= nums[l] or target <= nums[r]:
+                # divide and conquer
+                m = (l + r) // 2
+                return max(find(l, m), find(m+1, r))
+            return -1
+
+        return find(0, n-1)
 
 if __name__ == '__main__':
     #input = sys.stdin.read()
