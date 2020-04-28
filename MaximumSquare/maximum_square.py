915ca744a4f5a4dda9ce696565345e242136aea0
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-04-28 12:31:48 +1000

    maximum sqaure

---
+++ MaximumSquare/maximum_square.py
@@ -0,0 +1,26 @@
+import sys 
+from typing import List 
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        n, m = len(matrix), len(matrix[0])
+        mat2 = [[0 for _ in range(m + 1)] for __ in range(n + 1)]
+        for i in range(n):
+            for j in range(m):
+                mat2[i][j] = min(mat2[i - 1][j - 1], mat2[i - 1][j], mat2[i][j - 1]) + 1 if matrix[i][j] == '1' else 0
+        return max([max(row) for row in mat2]) ** 2
+
+if __name__ == '__main__':
+    #input = sys.stdin.read()
+    #nums = list(map(int, input.split()))
+    """
+    matrix = [['0','1','1','1','0'],
+            ['1','1','1','1','1'],
+            ['0','1','1','1','1'],
+            ['0','1','1','1','1']]
+    """
+    matrix = [["0","1"], ["1","0"]]
+    sol = Solution()
+    print(sol.maximalSquare(matrix))
