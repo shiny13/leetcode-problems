f571bd5d4697f5fa011763b0e0c2437c3914a508
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-06-05 21:34:09 +1000

    random pick with weight

---
+++ RandomPickWithWeight/random_pick_with_weight.py
@@ -0,0 +1,31 @@
+import sys 
+import random 
+from typing import List 
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefix_sum = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sum.append(prefix_sum)
+        self.total_sum = prefix_sum 
+
+    def pickIndex(self) -> int:
+        random_num = self.total_sum * random.random()
+        low, high = 0, len(self.prefix_sum)
+        while low < high:
+            mid = low + (high-low)//2
+            if random_num > self.prefix_sum[mid]:
+                low = mid + 1
+            else:
+                high = mid 
+        return low 
+
+
+"""
+if __name__ == '__main__':
+    obj = Solution(w)
+    param_1 = obj.pickIndex()
+"""
