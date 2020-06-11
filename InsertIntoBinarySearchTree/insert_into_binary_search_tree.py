fee4b87ad46af58f8dc7fb775a8c11c4a608b754
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-06-11 22:34:16 +1000

    insert into binary search tree

---
+++ InsertIntoBinarySearchTree/insert_into_binary_search_tree.py
@@ -0,0 +1,45 @@
+import sys 
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        """
+        def insert(node: TreeNode, val: int):
+            if node is None:
+                node = TreeNode(val) 
+                return
+            if node.val is None:
+                node.val = val 
+                return
+
+            if node.val > val and node.left is None:
+                node.left = TreeNode(val)
+                return
+            elif node.val > val and node.left is not None:
+                insert(node.left, val)
+            
+            if node.val < val and node.right is None:
+                node.right = TreeNode(val)
+                return
+            elif node.val < val and node.right is not None:
+                insert(node.right, val)
+
+        insert(root, val)
+        return root 
+        """
+        def recur(u):
+            if not u: return TreeNode(val)
+            if val < u.val: u.left  = recur(u.left)
+            else:           u.right = recur(u.right)
+            return u
+        return recur(root)
+"""
+if __name__ == '__main__':
+    
+    sol = Solution()
+"""
