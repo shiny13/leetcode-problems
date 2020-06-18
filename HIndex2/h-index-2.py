a505b366e6392df9a5dbc0e037c6e27857c71897
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-06-18 23:10:03 +1000

    h index 2

---
+++ HIndex2/h-index-2.py
@@ -0,0 +1,27 @@
+import sys 
+from typing import List 
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        if citations is None or len(citations) == 0: 
+            return 0
+        i, n, res = 0, len(citations), 0
+        mid, start, end = 0, 0, len(citations) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if (citations[mid] == n - mid):
+                return n - mid
+            elif citations[mid] < n - mid:
+                start = mid + 1
+            else:
+                res = n - mid 
+                end = mid - 1 
+
+        return res
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    citations = list(map(int, input.split()))
+    
+    sol = Solution()
+    print(sol.hIndex(citations))
