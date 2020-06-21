d3e0b5a7bb5a076d06359f901fef9ad0d7b9a3a0
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-06-21 12:26:43 +1000

    binary tree pruning

---
+++ BinaryTreePruning/binary_tree_pruning.py
@@ -0,0 +1,34 @@
+import sys 
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+
+        def prune(node: TreeNode):
+            if not node: return False 
+            a1 = prune(node.left)
+            a2 = prune(node.right)
+            if not a1: 
+                node.left = None 
+            if not a2:
+                node.right = None 
+
+        return root if prune(root) else None
+
+if __name__ == '__main__':
+
+    node = TreeNode(1)
+    node.left = TreeNode(0)
+    node.left.left = TreeNode(0)
+    node.left.right = TreeNode(0)
+    node.right = TreeNode(1)
+    node.right.left = TreeNode(0)
+    node.right.right = TreeNode(1)
+
+    sol = Solution()
+    sol.pruneTree(node)
