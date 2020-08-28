28522cfb7601462f2d4618ef8e2248ff6b562178
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-08-28 15:15:21 +1000

    fizzbuzz

---
+++ FizzBuzz/fizzbuzz.py
@@ -0,0 +1,35 @@
+import sys 
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+
+        for num in range(1,n+1):
+
+            divisible_by_3 = (num % 3 == 0)
+            divisible_by_5 = (num % 5 == 0)
+
+            num_ans_str = ""
+
+            if divisible_by_3:
+                # Divides by 3
+                num_ans_str += "Fizz"
+            if divisible_by_5:
+                # Divides by 5
+                num_ans_str += "Buzz"
+            if not num_ans_str:
+                # Not divisible by 3 or 5
+                num_ans_str = str(num)
+
+            # Append the current answer str to the ans list
+            ans.append(num_ans_str)  
+
+        return ans
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input.strip())
+
+    sol = Solution
+    print(sol.fizzBuzz)
