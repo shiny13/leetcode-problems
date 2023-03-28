95cdf1b146d6b096822c0fbfcced6517a9eb5061
shiny13 <shahnawaz.alam13@gmail.com>
2023-03-28 23:11:40 +1100

    271. Contains Duplicates

---
+++ Data-Structures-study/ContainsDuplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContainsDuplicate;
+
+Console.WriteLine("271. Contains Duplicate");
+Console.WriteLine("-----------------------------------------");
+
+var sol = new Solution();
+bool result;
+
+Console.WriteLine("[1,2,3,1]");
+result = sol.ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+Console.WriteLine($"result: {result}");
+
+Console.WriteLine("[1,2,3,4]");
+result = sol.ContainsDuplicate(new int[] { 1, 2, 3, 4 });
+Console.WriteLine($"result: {result}");
+
+Console.WriteLine("[1,1,1,3,3,4,3,2,4,2]");
+result = sol.ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 });
+Console.WriteLine($"result: {result}");
---
+++ Data-Structures-study/ContainsDuplicate/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsDuplicate
+{
+    public class Solution
+    {
+        public Solution () { }
+
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var isDuplicate = false;
+            var hashMap = new Dictionary<int, bool>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashMap.ContainsKey(nums[i]))
+                {
+                    isDuplicate = true;
+                    break;
+                }
+                hashMap.Add(nums[i], true);
+            }
+
+            return isDuplicate;
+        }
+    }
+}
