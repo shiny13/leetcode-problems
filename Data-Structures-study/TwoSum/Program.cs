b55c3d2d0aa19dead6290b24f8f1252170f8cf88
shiny13 <shahnawaz.alam13@gmail.com>
2023-03-31 01:57:42 +1100

    1. Two Sum

---
+++ Data-Structures-study/TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TwoSum;
+
+Console.WriteLine("1. Two Sum");
+Console.WriteLine("--------------------------------");
+
+var sol = new Solution();
+
+Console.WriteLine("nums: [2,7,11,15] | target: 9");
+var result = sol.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+var resultStr = string.Join(", ", result);
+Console.WriteLine($"Output: [{resultStr}]");
+
+Console.WriteLine("nums: [3,2,4] | target: 6");
+result = sol.TwoSum(new int[] { 3, 2, 4 }, 6);
+resultStr = string.Join(", ", result);
+Console.WriteLine($"Output: [{string.Join(", ", sol.TwoSum(new int[] { 3, 2, 4 }, 6))}]");
+
+Console.WriteLine("nums: [3,3] | target: 6");
+result = sol.TwoSum(new int[] { 3, 3 }, 6);
+resultStr = string.Join(", ", result);
+Console.WriteLine($"Output: [{string.Join(", ", sol.TwoSum(new int[] { 3, 3 }, 6))}]");
---
+++ Data-Structures-study/TwoSum/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public Solution() { }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var hasMap = new Dictionary<int, int>();
+            var result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var difference = target - nums[i];
+                if (hasMap.ContainsKey(difference))
+                {
+                    result[0] = hasMap[difference];
+                    result[1] = i;
+                    break;
+                }
+                hasMap.Add(nums[i], i);
+            }
+
+            return result;
+        }
+    }
+}
