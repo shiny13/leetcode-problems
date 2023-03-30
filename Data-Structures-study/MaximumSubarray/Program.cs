e8b875e3d1ab6a6ddfd41d6790974e3d03cf61d3
shiny13 <shahnawaz.alam13@gmail.com>
2023-03-31 01:21:50 +1100

    53. Maximum Subarray

---
+++ Data-Structures-study/MaximumSubarray/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MaximumSubarray;
+
+Console.WriteLine("53. Maximum Subarray");
+Console.WriteLine("--------------------------------------");
+
+var sol = new Solution();
+
+Console.WriteLine("[-2,1,-3,4,-1,2,1,-5,4]");
+Console.WriteLine($"Output: {sol.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 })}");
+
+Console.WriteLine("[1]");
+Console.WriteLine($"Output: {sol.MaxSubArray(new int[] { 1 })}");
+
+Console.WriteLine("[5,4,-1,7,8]");
+Console.WriteLine($"Output: {sol.MaxSubArray(new int[] { 5, 4, -1, 7, 8 })}");
---
+++ Data-Structures-study/MaximumSubarray/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumSubarray
+{
+    public class Solution
+    {
+        public Solution() { }
+
+        public int MaxSubArray(int[] nums)
+        {
+            var max_so_far = Int32.MinValue;
+            var max_ending_here = 0;
+
+            foreach (int x in nums)
+            {
+                max_ending_here += x;
+                if (max_so_far < max_ending_here)
+                {
+                    max_so_far = max_ending_here;
+                }
+                if (max_ending_here < 0)
+                {
+                    max_ending_here = 0;
+                }
+            }
+
+            return max_so_far;
+        }
+    }
+}
