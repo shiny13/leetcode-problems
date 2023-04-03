28abc457fd998769e32d8802e727adec7e4744ef
shiny13 <shahnawaz.alam13@gmail.com>
2023-04-04 01:42:40 +1000

    88. Merged sort array

---
+++ Data-Structures-study/MergeSortedArray/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MergeSortedArray;
+Console.WriteLine("88. Merge Sorted Array");
+Console.WriteLine("-------------------------------------------");
+
+var sol = new Solution();
---
+++ Data-Structures-study/MergeSortedArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortedArray
+{
+    public class Solution
+    {
+        public Solution() { }
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m; i < m+n; i++)
+            {
+                nums1[i] = nums2[i-m];
+            }
+            Array.Sort(nums1);
+        }
+    }
+}
