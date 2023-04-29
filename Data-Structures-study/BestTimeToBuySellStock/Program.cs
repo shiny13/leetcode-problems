48d587962d5e0fcc3f93d99e4a395183ab058ed7
shiny13 <shahnawaz.alam13@gmail.com>
2023-04-29 22:04:15 +1000

    [master] Added Object type exercise

---
+++ Data-Structures-study/BestTimeToBuySellStock/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("121. Best time to buy and sell stock");
---
+++ Data-Structures-study/BestTimeToBuySellStock/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuySellStock
+{
+    public class Solution
+    {
+        public Solution() { }
+
+        public int MaxProfit(int[] prices)
+        {
+            var profit = 0;
+            var difference = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (i + 1 == prices.Length)
+                    break;
+                difference = prices[i+1] - prices[i];
+                if (difference > 0)
+                {
+                    if (difference > profit)
+                        profit = difference;
+                }
+                else
+                {
+                    difference = 0;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
